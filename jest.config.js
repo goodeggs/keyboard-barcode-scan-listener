672803890e21d12d1ded66775fbae42c472ada58
Nathan Houle <nathan@nathanhoule.com>
2019-01-09 12:18:56 -0800

    chore(ci): Generate coverage reports in CI and upload to Codecov

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,40 @@
+const stringToBoolean = (val) => {
+  if (val == null) {
+    return false;
+  }
+
+  if (typeof val !== 'string') {
+    throw new Error(`Cannot parse value "${val}" (expected a string but received a ${typeof val})`);
+  }
+
+  switch (val.trim().toLowerCase()) {
+    case '1':
+    case 'true':
+      return true;
+    case '0':
+    case 'false':
+      return false;
+  }
+
+  throw new Error(`Could not parse unexpected value "${val}" as a boolean`);
+};
+
 /**
  * Jest configuration for running for unit tests.
+ *
+ * Coverage reporting is automatically enabled in CI environments; enable it locally by setting
+ * `COVERAGE=true`.
  */
-
 module.exports = {
-  testURL: 'http://localhost',
+  collectCoverage: stringToBoolean(process.env.CI) || stringToBoolean(process.env.COVERAGE),
+  collectCoverageFrom: ['**/*.{ts,tsx,js,jsx}'],
+  coverageDirectory: './.coverage/',
+  coveragePathIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/jest.config.js'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
   testMatch: ['**/?(*.)test.!(*.){js,jsx,ts,tsx}'],
   testPathIgnorePatterns: ['<rootDir>/.coverage/', '<rootDir>/lib/', '<rootDir>/node_modules/'],
+  testURL: 'http://localhost',
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
-  coverageDirectory: './.coverage/',
-  collectCoverageFrom: ['**/*.{ts,tsx,js,jsx}'],
-  coveragePathIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/jest.config.js'],
 };
